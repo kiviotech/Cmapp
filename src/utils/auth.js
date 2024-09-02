a99b68d80a703244842b331d63a5f2b101427a3e
Adityasiingh14 <Adityasiingh14@gmail.com>
2024-09-02 17:57:40 +0530

    login setup done.

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,21 +1,27 @@
 import apiClient from '../api/apiClient';
+import { deleteToken, saveToken } from './storage';
 
 export const login = async (email, password) => {
   try {
-    const response = await apiClient.post('/auth/local', {
+    const response = await apiClient.post("/auth/local", {
       identifier: email,
       password: password,
     });
 
-    localStorage.setItem('authToken', response.data.jwt);
+    // Extract JWT and user data from the response
+    const { jwt, user } = response.data;
+
+    console.log("auth.js", jwt);
+    // Save the JWT to secure storage
+    saveToken(jwt);
     return response.data;
   } catch (error) {
     throw error;
   }
 };
 
-export const logout = () => {
-  localStorage.removeItem('authToken');
+export const logout = (jwt) => {
+  deleteToken(jwt);
 };
 
 export const SignUp = async (email, password) => {
