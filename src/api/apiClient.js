9d6c70ece51698fba0032b69d332b8e1862294ec
root <root@e2e-101-241.ssdcloudindia.net>
2024-11-05 16:39:10 +0530

    chnaged the token function to get api token intsed of hardcoded value

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -13,7 +13,8 @@ const apiClient = axios.create({
 
 apiClient.interceptors.request.use(
   config => {
-    const token = getToken(); // Call the function to get the token
+    //const token = getToken(); // Call the function to get the token
+    const token = "877cd671cf75b4ad4afde7686487f9f510b2283daab876d6d321adc74b4ae954d173b70a5725f4785f88ee735b6807fbddd9a5d88697d8bb304dd02913bc735f71362403014dcc25b0340769f4c5da41ec203153868394808fac143f167988ef537bf218cd51f15a07cd9225e773eb2aa7e238882f0870bc950ae70bc5336fb2"
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
